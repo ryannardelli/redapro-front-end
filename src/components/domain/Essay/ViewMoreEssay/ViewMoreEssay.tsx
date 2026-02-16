90f3e396ed4fb41b0384b6bfb9df3ed8c6e55a59
Ryan Nardelli <ryannardelli12@gmail.com>
2026-02-15 22:28:10 -0300

    perf: Improving reusable about components

--- src/components/domain/Essay/ViewMoreEssay/ViewMoreEssay.tsx
+++ src/components/domain/Essay/ViewMoreEssay/ViewMoreEssay.tsx
@@ -1,56 +1,7 @@
-// import { ModalBase } from "@components/ui/Modal/ModalBase";
-// import { Eye } from "lucide-react";
-// import { useState } from "react";
-// import { ExibitionMoreEssay } from "../ExibitionMoreEssay";
-
-// interface ViewMoreEssayProps {
-//   onView: () => void;
-//   loading?: boolean;
-//   title?: string;
-//   className?: string;
-// }
-
-// export function ViewMoreEssay({
-//   onView,
-//   loading = false,
-//   title = "Ver redação completa",
-//   className = "",
-// }: ViewMoreEssayProps) {
-//   const [isOpen, setIsOpen] = useState(false);
-//   return (
-//     <>
-//     <button
-//       onClick={onView}
-//       disabled={loading}
-//       title={title}
-//       className={`
-//         p-2 rounded-lg shadow-md transition
-//         ${
-//           loading
-//             ? "bg-gray-200 text-gray-400 cursor-not-allowed"
-//             : "bg-white/90 hover:bg-indigo-50 text-gray-400 hover:text-indigo-600 cursor-pointer"
-//         }
-//         ${className}
-//       `}
-//     >
-//       <Eye size={18} />
-//     </button>
-
-//      <ModalBase
-//         title="Editar Redação"
-//         isOpen={isOpen}
-//         onClose={() => setIsOpen(false)}
-//       >
-//         <ExibitionMoreEssay />
-//       </ModalBase>
-//     </>
-//   );
-// }
-
-import { ModalBase } from "@components/ui/Modal/ModalBase";
 import { Eye } from "lucide-react";
 import { useState } from "react";
 import { ExibitionMoreEssay } from "../ExibitionMoreEssay";
+import { ModalShowBase } from "@components/ui/Modal/ModalShowBase";
 
 interface ViewMoreEssayProps {
   essay: any;
@@ -82,7 +33,7 @@ export function ViewMoreEssay({
         <Eye size={18} />
       </button>
 
-      <ModalBase
+      <ModalShowBase
         title="Visualização da Redação"
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
@@ -92,7 +43,7 @@ export function ViewMoreEssay({
           category={essay.category?.name} 
           content={essay.content} 
         />
-      </ModalBase>
+      </ModalShowBase>
     </>
   );
 }
