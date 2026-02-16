0e43ee59b004ae0008361a005ab7336b4d7c6b94
Ryan Nardelli <ryannardelli12@gmail.com>
2026-02-16 19:58:19 -0300

    feat: Created screen categories

--- src/routers/AdminSetupRoutes/index.tsx
+++ src/routers/AdminSetupRoutes/index.tsx
@@ -4,6 +4,7 @@ import { SetupHome } from "../../pages/PagesAdmin/AdminSetup/SetupHome";
 import { MenuBuilder } from "../../pages/PagesAdmin/AdminSetup/MenuBuilder";
 import { ProfileBuilder } from "../../pages/PagesAdmin/AdminSetup/ProfileBuilder";
 import ModelsEssay from "../../pages/PagesAdmin/AdminSetup/ModelsEssay";
+import { Categories } from "pages/PagesAdmin/AdminSetup/Categories";
 
 export function AdminSetupRoutes() {
   return (
@@ -11,6 +12,7 @@ export function AdminSetupRoutes() {
       <Route index element={<SetupHome />} />
       <Route path="menus" element={<MenuBuilder />} />
       <Route path="profiles" element={<ProfileBuilder />} />
+      <Route path="categories" element={<Categories />} />
       <Route path="reference-essay" element={<ModelsEssay />} />
     </Route>
   );
