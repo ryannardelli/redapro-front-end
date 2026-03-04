b2ed71bd892ade78f722a6e60a5a127fc411318f
Ryan Nardelli <ryannardelli12@gmail.com>
2026-03-04 14:45:10 -0300

    fix: fix made

--- src/components/domain/Profile/DeleteProfile/DeleteProfile.tsx
+++ src/components/domain/Profile/DeleteProfile/DeleteProfile.tsx
@@ -19,7 +19,7 @@ export function DeleteProfile({
         disabled={loading}
         title={title}
         className={`
-            p-2 rounded-lg shadow-md transition
+            p-2 text-gray-400 hover:text-red-600 hover:bg-red-50 rounded-lg transition-colors
             ${loading
             ? "bg-gray-200 text-gray-400 cursor-not-allowed"
             : "bg-white/90 hover:bg-red-50 text-gray-400 hover:text-red-600 cursor-pointer"}
